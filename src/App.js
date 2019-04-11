abf0497417a297cd48d857298d86748b2203ec59
Brenton <brenton.pratt@gmail.com>
2019-04-11 08:52:43 -0600

    Set up Router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from "axios";
 import ProductBox from './ProductBox';
 import SearchBar from './SearchBar';
+import{
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
+import Cart from './Cart'
+
 
 class App extends Component {
 
-    state = {
-        products: []
-    };
-
-    componentDidMount(){
-        axios.get('https://my-json-server.typicode.com/tdmichaelis/typicode/products')
-            .then((response) => {
-                this.setState({
-                    products: response.data
-                });
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
   render() {
 
-        let products = this.state.products;
-
     return (
-        <>
+        <Router>
+            <div>
+                <div className="ui menu">
+                    <Link to='/products' className='item'>
+                        Products
+                    </Link>
+                    <Link to='/search' className='item'>
+                        Search
+                    </Link>
+                    <Link to='/cart' className='item'>
+                        Shopping Cart
+                    </Link>
+                </div>
+                <Switch>
+                    <Route
+                        path='/products'
+                        render={(props) => <ProductBox {...props} />}
+                    />
+                    <Route path='/company/:companyID' render={() => (
+                        <div>Multiple Companies</div>
+                    )} />
+                    <Route path='/search' component={SearchBar} />
+                    <Route path='/cart' component={Cart}/>
+                    <Route render={() => (
+                        <div>404 NOT FOUND</div>
+                    )}/>
+                </Switch>
+            </div>
+        </Router>
+        /*<>
             <SearchBar productsToSearch={products}/>
             <ProductBox productsFromApi={products}/>
-        </>
+        </>*/
     );
   }
 }
--- src/ProductBox.js
+++ src/ProductBox.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import SearchBar from "./SearchBar";
+import axios from "axios";
 
 class ProductBox extends Component{
 
+    state = {
+        products: []
+    };
+
+    componentDidMount(){
+        axios.get('https://my-json-server.typicode.com/tdmichaelis/typicode/products')
+            .then((response) => {
+                this.setState({
+                    products: response.data
+                });
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     render() {
+
+        let products = this.state.products;
+
         return(
-            <div className='ui cards'>
-                {this.props.productsFromApi.map(product =>
-                    <div className='ui card' key={product.id}>
-                        <h2 className='ui header'>{product.title}</h2>
-                        <img src={product.img} alt='product' className='ui small image'/>
-                        <div>Product Details: {product.description}</div>
-                        <div>${product.price}</div>
-                        <div className='ui heart rating'>Rating: {product.rating}</div>
-                        <div>Product Category: {product.category}</div>
-                    </div>
-                )}
-            </div>
+            <>
+                <SearchBar productsToSearch={products}/>
+                <div className='ui cards'>
+                    {products.map(product =>
+                        <div className='ui card' key={product.id}>
+                            <h2 className='ui header'>{product.title}</h2>
+                            <img src={product.img} alt='product' className='ui small image'/>
+                            <div>Product Details: {product.description}</div>
+                            <div>${product.price}</div>
+                            <div className='ui heart rating'>Rating: {product.rating}</div>
+                            <div>Product Category: {product.category}</div>
+                        </div>
+                    )}
+                </div>
+            </>
         )
     }
 }
